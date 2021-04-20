20578778aebfdb48116579c8f9aa47c8f4be74a1
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-21 02:57:49 +0530

    Created Home and SearchBar component

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="root">
+    <div class="main">
+      <SearchBar />
+      <h1>Home</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchBar from "./SearchBar";
+
+export default {
+  name: "Home",
+  components: {
+    SearchBar,
+  },
+};
+</script>
+
+<style scoped>
+.root {
+  min-height: 100vh;
+  background-image: linear-gradient(to right, #89f7fe, #66a6ff);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.main {
+  min-height: 80vh;
+  background-color: rgb(217, 237, 255);
+  min-width: 80%;
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <form @submit="onSubmit" class="search-bar">
+    <div class="form-control">
+      <input
+        class="input"
+        type="text"
+        v-model="searchQuery"
+        name="searchQuery"
+        placeholder="Search cocktails"
+      />
+    </div>
+    <input type="submit" value="Search" class="search-btn" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      if (!this.searchQuery) {
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.search-bar {
+  display: flex;
+}
+.input {
+  border-width: 0;
+  outline-width: 0;
+  padding: 1rem;
+  font-size: 1rem;
+}
+.search-btn {
+  background-image: linear-gradient(to right, #89f7fe, #66a6ff);
+  border: none;
+  border-radius: 10rem;
+}
+</style>
