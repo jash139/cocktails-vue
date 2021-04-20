0b579abdb85d1420e5e6767a26b2d068b811d420
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-21 03:15:58 +0530

    Styled the SearchBar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,14 +1,12 @@
 <template>
   <form @submit="onSubmit" class="search-bar">
-    <div class="form-control">
-      <input
-        class="input"
-        type="text"
-        v-model="searchQuery"
-        name="searchQuery"
-        placeholder="Search cocktails"
-      />
-    </div>
+    <input
+      class="search-field"
+      type="text"
+      v-model="searchQuery"
+      name="searchQuery"
+      placeholder="Search cocktails"
+    />
     <input type="submit" value="Search" class="search-btn" />
   </form>
 </template>
@@ -33,17 +31,40 @@ export default {
 
 <style scoped>
 .search-bar {
+  background-color: white;
+  border-radius: 10rem;
   display: flex;
+  max-width: 25rem;
 }
-.input {
-  border-width: 0;
-  outline-width: 0;
-  padding: 1rem;
-  font-size: 1rem;
+.search-field {
+  border: none;
+  border-radius: 10rem;
+  color: #66a6ff;
+  font-size: 1.2rem;
+  outline: none;
+  padding: 0.5rem 1.5rem;
+  width: 100%;
+}
+::placeholder {
+  color: #cce2ff;
+}
+:-ms-input-placeholder {
+  color: #cce2ff;
+}
+::-ms-input-placeholder {
+  color: #cce2ff;
 }
 .search-btn {
   background-image: linear-gradient(to right, #89f7fe, #66a6ff);
-  border: none;
+  border-width: 0;
   border-radius: 10rem;
+  outline-width: 0;
+  padding: 1rem;
+  width: 10rem;
+  transition: all 0.2s ease;
+}
+.search-btn:hover {
+  cursor: pointer;
+  transform: scale(1.08);
 }
 </style>
