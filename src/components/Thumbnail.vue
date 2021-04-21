ad671c960271c512312ccd13de1750b1fd95a660
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-21 20:00:54 +0530

    Creatd Thumbnail component

---
+++ src/components/Thumbnail.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    class="thumbnail"
+    src="https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1125&q=80"
+    alt="cocktail"
+  />
+</template>
+
+<script>
+export default {
+  name: "Thumbnail",
+};
+</script>
+
+<style scoped>
+.thumbnail {
+}
+</style>
