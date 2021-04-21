dbe1bff783f952850c3ead51bd3525638522b726
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-21 20:16:49 +0530

    Added styling to Thumbnail component

--- src/components/Thumbnail.vue
+++ src/components/Thumbnail.vue
@@ -5,7 +5,9 @@
       src="https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1125&q=80"
       alt="cocktail"
     />
-    <h3 class="cocktail-name">Margarita</h3>
+    <div class="cocktail-name-div">
+      <h3 class="cocktail-name">Margarita</h3>
+    </div>
   </div>
 </template>
 
@@ -17,9 +19,26 @@ export default {
 
 <style scoped>
 .thumbnail {
+  background-color: #89f7fe;
+  position: relative;
+  max-width: 500px;
 }
 .cocktail-image {
+  width: 100%;
+}
+.cocktail-name-div {
+  background-image: linear-gradient(to right, #89f7fe, #66a6ff);
+  border-radius: 8px;
+  padding: 0.5rem 1.5rem;
+  position: absolute;
+  left: 50%;
+  bottom: 2rem;
+  transform: translateX(-50%);
 }
 .cocktail-name {
+  color: white;
+  font-family: "Tangerine", cursive;
+  font-size: 2rem;
+  font-weight: 700;
 }
 </style>
