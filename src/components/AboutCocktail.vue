b6644a2ff736d4a806d111f167cfc1b9ae7afe0b
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-21 21:48:21 +0530

    Created AboutCocktail component

---
+++ src/components/AboutCocktail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about-cocktail">
+    <p class="instruction">
+      Rub the rim of the glass with the lime slice to make the salt stick to it.
+      Take care to moisten only the outer rim and sprinkle the salt on it. The
+      salt should present to the lips of the imbiber and never mix into the
+      cocktail. Shake the other ingredients with ice, then carefully pour into
+      the glass.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutCocktail",
+};
+</script>
+
+<style scoped>
+</style>
