155ab62821929a77e3906041b45a4a3a5fbbfd98
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-21 23:32:48 +0530

    Created Socials component with social buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,21 @@
   <header class="header">
     <LogoIllustration />
     <SearchBar />
+    <Socials />
   </header>
 </template>
 
 <script>
 import LogoIllustration from "./LogoIllustration";
 import SearchBar from "./SearchBar";
+import Socials from "./Socials";
 
 export default {
   name: "Header",
   components: {
     LogoIllustration,
     SearchBar,
+    Socials,
   },
 };
 </script>
---
+++ src/components/Socials.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="socials">
+    <div class="social-button">
+      <LinkedinSocial />
+    </div>
+    <div class="social-button">
+      <GithubSocial />
+    </div>
+    <div class="social-button">
+      <EmailSocial />
+    </div>
+  </div>
+</template>
+
+<script>
+import LinkedinSocial from "./LinkedinSocial";
+import GithubSocial from "./GithubSocial";
+import EmailSocial from "./EmailSocial";
+
+export default {
+  name: "Socials",
+  components: {
+    LinkedinSocial,
+    GithubSocial,
+    EmailSocial,
+  },
+};
+</script>
+
+<style scoped>
+.social-button {
+  border-radius: 10rem;
+  display: inline-flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  padding: 0.9rem;
+  height: 50px;
+  width: 50px;
+}
+.social-button:hover {
+  background-color: #66a6ff1e;
+}
+</style>
