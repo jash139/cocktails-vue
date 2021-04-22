50c075da46f19945fc0723d5ab92c897a12ddb3c
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-22 21:29:33 +0530

    Created MenuItem as separate component

--- src/components/Cocktails.vue
+++ src/components/Cocktails.vue
@@ -2,28 +2,30 @@
   <div class="cocktails-div">
     <Subheading text="Drinks & Cocktails" />
     <div class="cocktails-menu">
-      <div class="menu-item">
-        <img
-          class="cocktail-avatar"
-          src="https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1125&q=80"
-          alt="cocktail"
-        />
-        <div class="about-cocktail">
-          <h4>Cocktail 1</h4>
-          <p>Alcoholic</p>
-        </div>
-      </div>
+      <MenuItem :cocktailDetails="cocktailDetails" />
     </div>
   </div>
 </template>
 
 <script>
 import Subheading from "./Subheading";
+import MenuItem from "./MenuItem";
 
 export default {
   name: "Cocktails",
+  data() {
+    return {
+      cocktailDetails: {
+        thumbnail:
+          "https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1125&q=80",
+        name: "Margarita",
+        isAlcoholic: "Non Alcoholic",
+      },
+    };
+  },
   components: {
     Subheading,
+    MenuItem,
   },
 };
 </script>
@@ -32,18 +34,6 @@ export default {
 .cocktails-div {
   background-color: rgba(255, 255, 255, 0.7);
 }
-.menu-item {
-  display: flex;
-  align-items: center;
-}
-.menu-item:hover {
-  background-color: rgb(217, 237, 255);
-  cursor: pointer;
-}
-.cocktail-avatar {
-  border-radius: 10rem;
-  height: 60px;
-  margin: 0 1.5rem;
-  width: 60px;
+.cocktails-menu {
 }
 </style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="menu-item">
+    <img
+      class="cocktail-avatar"
+      :src="cocktailDetails.thumbnail"
+      alt="cocktail"
+    />
+    <div class="about-cocktail">
+      <h4 class="cocktail-name">{{ cocktailDetails.name }}</h4>
+      <p class="is-alcoholic">{{ cocktailDetails.isAlcoholic }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuItem",
+  props: {
+    cocktailDetails: Object,
+  },
+};
+</script>
+
+<style scoped>
+.menu-item {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+}
+.menu-item:hover {
+  background-color: rgb(217, 237, 255);
+  cursor: pointer;
+}
+.cocktail-avatar {
+  border-radius: 10rem;
+  height: 60px;
+  margin: 0 1rem;
+  width: 60px;
+}
+.about-cocktail {
+}
+.cocktail-name {
+  color: #66a6ff;
+  font-size: 1.2rem;
+  font-weight: 700;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 150px;
+}
+.is-alcoholic {
+  color: #60728a;
+  font-size: 1rem;
+  font-weight: 400;
+}
+</style>
