a462928518ba591081405fc0cce563b298b6f28e
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-25 17:24:16 +0530

    Added cocktails module to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import Vuex from "vuex";
 import Vue from "vue";
+import cocktails from "./modules/cocktails";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-
+        cocktails
     }
 });
