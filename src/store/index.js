b28c27eca0b2c59e51802c42fcf8760222b362f8
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-25 18:39:33 +0530

    Created ingredients module, added to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vuex from "vuex";
 import Vue from "vue";
+
 import cocktails from "./modules/cocktails";
+import ingredients from "./modules/ingredients";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        cocktails
+        cocktails,
+        ingredients
     }
 });
