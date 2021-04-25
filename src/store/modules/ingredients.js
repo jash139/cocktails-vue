42a83f250a1a1114250de616fade863fe4fb72be
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-25 19:57:12 +0530

    Added action, mutation to get ingredient details

--- src/store/modules/ingredients.js
+++ src/store/modules/ingredients.js
@@ -1,14 +1,27 @@
-// import axios from "axios";
+import axios from "axios";
 
 const state = {
-
+    ingredient: {},
 };
 
-const getters = {};
+const getters = {
+    ingredient: state => state.ingredient,
+};
 
-const actions = {};
+const actions = {
+    async getIngredient({ commit }, name) {
+        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${name}`);
+        const ingredientDetails = {
+            ...response.data.ingredients,
+            thumbnail: "https://www.thecocktaildb.com/images/ingredients/" + name + ".png"
+        };
+        commit("setIngredient", ingredientDetails);
+    }
+};
 
-const mutations = {};
+const mutations = {
+    setIngredient: (state, ingredient) => state.ingredient = ingredient
+};
 
 export default {
     state,
