a57ff423e3a33996cf178476b45d605d170d1080
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-26 19:15:15 +0530

    Added favicon

--- src/store/modules/cocktails.js
+++ src/store/modules/cocktails.js
@@ -5,9 +5,9 @@ const state = {
     activeCocktail: {
         strDrinkThumb: "https://images.unsplash.com/photo-1569929233287-f0565228c4d4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
         strDrink: "Select Drink",
-        strInstructions: "-"
+        strInstructions: ""
     },
-    ingredients: ["Select a drink"],
+    ingredients: [],
 };
 
 const getters = {
